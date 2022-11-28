91d5e6065b4378276229d760df7de5308e69936a
Steven G. Harris <steven.g.harris@gmail.com>
2022-11-28 15:03:27 -0800

    Initial commit

--- KeydownTest/ContentView.swift
+++ KeydownTest/ContentView.swift
@@ -6,21 +6,42 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct ContentView: View {
+    
+    // This html content, consisting of a contenteditable DIV never triggers the
+    // `keydown` event on Mac Catalyst for MacOS 13 (Ventura). It works fine on earlier
+    // MacOS versions, and it works fine on iOS targets. The `click` event triggers
+    // properly in all cases. You can view the logging to the console using the
+    // Safari Web Inspector. FWIW, the same contenteditable DIV works fine in Safari
+    // itself, triggering both events. It only appears to be an issue with WKWebView
+    // in Mac Catalyst as of MacOS 13. The same issue occurs in UIKit with direct usage
+    // of WKWebView.
+    var html: String = "<div contenteditable=\"true\" onkeydown=\"console.log('keydown: ' + event.key);\" onclick=\"console.log('click');\"<h1>Hello world.</h1></div>"
+    
+    // Alternatively use an input element, which also fails on Mac Catalyst for MacOS 13 (Ventura)
+    // var html: String = "<input type=\"text\" onkeydown=\"console.log('keydown: ' + event.key);\" onclick=\"console.log('click');\">"
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            WebView(html: html)
         }
         .padding()
+        .background(Color.red)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct WebView: UIViewRepresentable {
+    
+    var html: String
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        webView.loadHTMLString(html, baseURL: nil)
     }
 }
+
